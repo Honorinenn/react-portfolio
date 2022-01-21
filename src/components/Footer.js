cd81e30ba095dd92ca53e0d7768d8c4f7c9d9452
Honorine Ndom <honorinendzah@gmail.com>
2022-01-20 17:18:00 -0800

    changes done on component css and js files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Footer.css'
 
 class Footer extends Component {
   render() {
@@ -19,8 +20,8 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2022 Nordic Giant</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; Copyright 2022 </li>
+              <li>Honorine Ndom Ndzah </li>
            </ul>
 
         </div>
--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Testimonial.css'
 
 class Testimonials extends Component {
   render() {
