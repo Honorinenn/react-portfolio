d1dfc424b5626409646f51a739701b998a2e38e0
Honorine Ndom <honorinendzah@gmail.com>
2022-02-05 07:56:00 -0800

    changes done on About.js, Contact.css, Portfolio.css,Resume.css

--- src/components/About.js
+++ src/components/About.js
@@ -40,9 +40,7 @@ class About extends Component {
 					   </p>
                </div>
                <div className="columns download">
-                  <p>
                      <a href={resumeDownload} className="button" target="_blank"><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
                </div>
             </div>
          </div>
