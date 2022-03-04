a2162cd982a184c3ca8952490cae458e200fcdcd
Honorine Ndom <honorinendzah@gmail.com>
2022-03-04 15:49:12 -0800

    changes done on resumeData.json

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,14 +3,14 @@ import './Resume.css'
 
 class Resume extends Component {
 
-  getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
+  // getRandomColor() {
+  //   var letters = '0123456789ABCDEF';
+  //   var color = '#';
+  //   for (var i = 0; i < 6; i++) {
+  //     color += letters[Math.floor(Math.random() * 16)];
+  //   }
+  //   return color;
+  // }
   
   render() {
 
